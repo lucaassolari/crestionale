16eaeb56df9846af3f05a4b5c7be9f0cb5164293
Luca <luca.assolari405@gmail.com>
2021-10-07 19:18:10 +0200

    Added Nodemon support and login validators.

--- code/crestionale/src/app/login-page/login-page.component.ts
+++ code/crestionale/src/app/login-page/login-page.component.ts
@@ -11,19 +11,51 @@ export class LoginPageComponent implements OnInit {
 
   loginUserData: any = {}
   private loginStatusListenerSubs: Subscription
+  emailIsInvalid = false
+
+  userNotExists = false
+  passwordWrong = false
   
   constructor(private _auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  validateEmail(email) {
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/
+
+    if(email.match(mailformat))
+      return true
+    else
+      return false
+  }
+
   loginUser() {
-    this.loginStatusListenerSubs = this._auth.loginUser(this.loginUserData) 
-    .subscribe(res => {
-      if(res)
-        window.alert(res)
-        
-      this.loginStatusListenerSubs.unsubscribe()
-    }) 
+
+    this.emailIsInvalid = false
+    this.userNotExists = false
+    this.passwordWrong = false
+
+    if(!this.validateEmail(this.loginUserData.email)) {
+      this.emailIsInvalid = true
+    }
+    else {
+      this.emailIsInvalid = false
+      this.loginStatusListenerSubs = this._auth.loginUser(this.loginUserData) 
+      .subscribe(res => {
+        if(res) {
+          if(res == 'User not exists')
+            this.userNotExists = true
+          else if(res == 'Password wrong')
+            this.passwordWrong = true
+          else
+            window.alert('Ooops! Qualcosa è andato storto')
+        }
+          
+        this.loginStatusListenerSubs.unsubscribe()
+      }) 
+    }
+
+    
   }
 }
