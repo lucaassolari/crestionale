c57fce2e5978c9500888ddc44b01a32efda8825a
Luca <luca.assolari405@gmail.com>
2022-01-21 15:07:42 +0100

    Add tests and fix users quotes

--- code/crestionale/src/app/navbar/navbar.component.spec.ts
+++ code/crestionale/src/app/navbar/navbar.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { NavbarComponent } from './navbar.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { DebugElement } from '@angular/core'
+import { By } from '@angular/platform-browser'
+import { NavbarComponent } from './navbar.component'
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+    let component: NavbarComponent
+    // Una fixture è una reference al componente con in più i campi e i metodi
+    let fixture: ComponentFixture<NavbarComponent>
+    let de: DebugElement
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
+            declarations: [NavbarComponent]
+        }).compileComponents()
+    }))
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NavbarComponent)
+        component = fixture.componentInstance
+        de = fixture.debugElement
+
+        fixture.detectChanges()
     })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it('should have "Entra" button if user is not authenticated', () => {
+        expect(de.query(By.css('.button.is-primary')).nativeElement.innerText).toBe('Entra')
+    })
+
+    it('should have "Registrati" button if user is not authenticated', () => {
+        expect(de.query(By.css('.button.is-info')).nativeElement.innerText).toBe('Registrati')
+    })
+
+    it('should have "Esci" button if user is authenticated', () => {
+        component.userIsAuthenticated = true
+        fixture.detectChanges()
+        expect(de.query(By.css('.button.is-danger')).nativeElement.innerText).toBe('Esci')
+    })
+
+    it('should have dropdown if "Altro" button is hovered', () => {
+        de.query(By.css('#other-button')).triggerEventHandler('mouseover', {})
+        fixture.detectChanges()
+        const e = de.query(By.css('#other-dropdown')).nativeElement
+        expect(getComputedStyle(e).opacity).toEqual('1')
+    })
+})
