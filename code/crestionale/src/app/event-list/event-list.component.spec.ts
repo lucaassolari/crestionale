6e1a8a7b1235f4cdc81c4a13ebbe4da9812f4746
Luca <luca.assolari405@gmail.com>
2022-01-26 15:09:05 +0100

    Refactor EventList test

--- code/crestionale/src/app/event-list/event-list.component.spec.ts
+++ code/crestionale/src/app/event-list/event-list.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { DebugElement } from '@angular/core'
-import { By } from '@angular/platform-browser'
 import { EventListComponent } from './event-list.component'
 
 describe('EventListComponent', () => {
